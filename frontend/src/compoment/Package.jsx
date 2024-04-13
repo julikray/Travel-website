d1f5e405c1d3d6673808b5a6c9ed7c25829cd2e7
Juli <julik.roy@gmail.com>
2024-04-13 16:53:03 +0530

    package update

--- frontend/src/compoment/Package.jsx
+++ frontend/src/compoment/Package.jsx
@@ -1,177 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Package() {
+  const [noOfElement, setNoOfElement] = useState(6);
+
+  const loadMore = () => {
+    setNoOfElement(noOfElement + noOfElement); 
+  };
+
+   const data = [
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    {
+      image: "./image/tej",
+      heading: "adventure & tour",
+      para: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque",
+    },
+    
+  ];
   return (
     <div>
       <div className="aboutImg1">
         <img src="image/footer-bg.jpg" alt="" />
       </div>
 
-      <section class="home-packages">
+      <section className="home-packages">
         <h1>TOP DESTINATION</h1>
 
-        <div class="box-container">
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
+        <div className="box-container">
+          {data.slice(0, noOfElement).map((item, index) => (
+            <div className="box" key={index}>
+              <div className="image">
+                <img src={item.image} alt="" />
+              </div>
+              <div className="content">
+                <h3>{item.heading}</h3>
+                <p>{item.para}</p>
+                <Link to="/book" className="btn">
+                  book now
+                </Link>
+              </div>
             </div>
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
-
-          <div class="box">
-            <div class="image">
-              <img src="./image/tej" alt="" />
-            </div>
-            <div class="content">
-              <h3>adventure & tour</h3>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-                quaerat deleniti cupiditate
-              </p>
-              <Link to="/book" class="btn">
-                book now
-              </Link>
-            </div>
-          </div>
+          ))}
         </div>
-        <div class="load-more">
-          <Link to="/package" class="btn">
-            load more
-          </Link>
+        <div className="load-more">
+         
+          {noOfElement < data.length && (
+            <button onClick={loadMore} className="btn">
+              Load More
+            </button>
+          )}
         </div>
       </section>
-
-
     </div>
   );
 }
